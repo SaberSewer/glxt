e52c715cfcd285c7c6546786612ad1f3c28622c3
cangjie <317060952@qq.com>
2019-04-26 23:22:33 +0800

    内容管理

--- src/main/java/online/cangjie/action/ArticleAction.java
+++ src/main/java/online/cangjie/action/ArticleAction.java
@@ -1,11 +1,36 @@
 package online.cangjie.action;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+import online.cangjie.interfaces.service.ArticleTagService;
 
 /**
  * 
  * @author 仓颉
  * 文章相关的Action
  */
-public class ArticleAction {
+public class ArticleAction extends ActionSupport{
+	@Autowired
+	private ArticleTagService articleTagService;
+	
+	public String getTag(){
+		List<?> list = articleTagService.getArticleTag();
+		ActionContext.getContext().getSession().put("tag", list);
+		return "tag";
+	}
+
+	public ArticleTagService getArticleTagService() {
+		return articleTagService;
+	}
+
+	public void setArticleTagService(ArticleTagService articleTagService) {
+		this.articleTagService = articleTagService;
+	}
+	
 
 }
